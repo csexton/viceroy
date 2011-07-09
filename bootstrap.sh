5460b41ca780e3702ee8f436f82eded478bc68f0
Christopher Sexton <csexton@gmail.com>
2011-07-08 21:01:01 -0400

    Install viceroy but not plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,3 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 git clone git://github.com/csexton/viceroy.git ~/.vim
 for i in vimrc gvimrc; do [ -e $i ] && ln -s ~/.vim/$i ~/.$i; done
-vim +ViceroyInit
