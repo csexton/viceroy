3299a2d736650e6b3c587412ded2228151d15f5e
Christopher Sexton <csexton@gmail.com>
2011-07-18 16:39:28 -0400

    Fixed if typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ git clone http://github.com/csexton/viceroy.git ~/.vim
 cd ~/.vim
 for i in vimrc gvimrc; do [ -e $i ] && ln -s ~/.vim/$i ~/.$i; done
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-[ ! -e ~/.vimrc.local] && echo '" Add your customizations for vim in this file' > ~/.vimrc.local
+[ ! -e ~/.vimrc.local ] && echo '" Add your customizations for vim in this file' > ~/.vimrc.local
 touch ~/.vimrc.local
 cat ~/.vim/viceroy/welcome.txt
