da575783c1c6d2efee3d9f16361f60ffc752a453
Christopher Sexton <csexton@gmail.com>
2011-07-18 16:48:49 -0400

    Remove unnecessary line

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,4 @@ cd ~/.vim
 for i in vimrc gvimrc; do [ -e $i ] && ln -s ~/.vim/$i ~/.$i; done
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 [ ! -e ~/.vimrc.local ] && echo '" Add your customizations for vim in this file' > ~/.vimrc.local
-touch ~/.vimrc.local
 cat ~/.vim/viceroy/welcome.txt
