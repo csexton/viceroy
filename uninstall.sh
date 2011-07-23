b18adbe1260e768445b0d057917d5a411637f0c6
Colin MacDonald <colin@bluegraybox.com>
2011-07-23 09:51:39 -0400

    Fixed bug in uninstall script; tidied.
    Misplaced 'done' caused .vim to be moved into .vim.viceroy backup.
    Put "restoring" messages before action - safer.
    Added uninstall notice to welcome.

--- uninstall.sh
+++ uninstall.sh
@@ -2,14 +2,14 @@
 [ ! -e ~/.vimrc.old ] && echo "No old vim config to restore! (~/.vimrc.old)" && exit 1
 for i in ~/.vimrc ~/.gvimrc ; do
 [ -e $i ] && [ ! -h $i ] && echo "$i is not a symlink. Something is wrong!" && exit 1
+done
 
 i=~/.vim
-[ -e $i ] && echo "Backing up $i to $i.viceroy" && mv $i $i.viceroy && [ -e $i.old ] && mv $i.old $i && echo "$i.old restored"
-done
+[ -e $i ] && echo "Backing up $i to $i.viceroy" && mv $i $i.viceroy && [ -e $i.old ] && echo "Restoring $i.old to $i" && mv $i.old $i
 for i in ~/.vimrc ~/.gvimrc ; do
-[ -h $i ] && echo "Removing viceroy symlink $i" && rm $i && [ -e $i.old ] && mv $i.old $i && echo "$i.old restored"
+[ -h $i ] && echo "Removing viceroy symlink $i" && rm $i && [ -e $i.old ] && echo "Restoring $i.old to $i" && mv $i.old $i
 done
 
 echo "Viceroy uninstalled."
-echo "Test that vim works correctly before deleting viceroy backups"
-
+i=~/.vimrc.local ; [ -e $i ] && echo "$i left in place"
+echo "Test that vim works correctly before deleting viceroy backup in ~/.vim.viceroy"
