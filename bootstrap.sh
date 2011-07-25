3eddc98dfaebcff63cc7fad9d27e5bd13273a8db
Christopher Sexton <csexton@gmail.com>
2011-07-24 21:44:28 -0400

    Change clone url back to canonical repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 
 # Checkout viceroy from git into .vim
 # git clone http://github.com/csexton/viceroy.git ~/.vim
-git clone http://github.com/bluegraybox/viceroy.git ~/.vim
+git clone http://github.com/csexton/viceroy.git ~/.vim
 
 # Symlink the config files
 for i in vimrc gvimrc; do echo "Link $i" && ln -sf ~/.vim/$i ~/.$i; done
